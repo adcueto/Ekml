6b8baf4881a05165d2fcf7909469f05567df309a
Adrian Cueto <adrianjpca@gmail.com>
2019-10-17 12:24:30 -0500

    Create Notify.cs
    "Notify" class created

---
+++ Notify.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ekml
+{
+    class Notify:Form
+    {
+        public void NotiCreated()
+        {
+       
+            notifyIcon1.Icon = SystemIcons.Information;
+            notifyIcon1.BalloonTipTitle = "Export Kml";
+            notifyIcon1.BalloonTipText = "Kml File created";
+            notifyIcon1.ShowBalloonTip(1000);
+            notifyIcon1.Visible = true;
+        }
+    }
+}
